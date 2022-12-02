1b72015504628abf5c9f5bf68932bb5ac3141f81
Lorenzo Balestrini <lo.bale@libero.it>
2022-12-02 14:17:56 +0100

    correzione

--- src/it/develhope/classesandobjects/CompetitionRules.java
+++ src/it/develhope/classesandobjects/CompetitionRules.java
@@ -3,9 +3,9 @@ package it.develhope.classesandobjects;
 // singleton class
 public class CompetitionRules {
 
-    public String competitionRule1 = "Do not copy and paste from StackOverflow!";
-    public String competitionRule2 = "Learn every day!";
-    public String competitionRule3 =  "Be the best team!";
+    private String competitionRule1 = "Do not copy and paste from StackOverflow!";
+    private String competitionRule2 = "Learn every day!";
+    private String competitionRule3 =  "Be the best team!";
 
     public void printRules(){
         System.out.println(competitionRule1);
